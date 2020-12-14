504970c43e4ac850970a0cc7ee77fda11c614444
Cristian <35613226+cristianvii@users.noreply.github.com>
2020-12-14 01:38:06 -0500

    Add files via upload

--- giphy-news/news-api.js
+++ giphy-news/news-api.js
@@ -1,7 +1,7 @@
 function setEleDisplay(id, display) {
     var ele = document.getElementById(id);
     if (!ele ) {
-        console.error('Element wit id ' + id + ' not found in document.');
+        console.error('Element with id ' + id + ' not found in document.');
         return;
     }
     ele.style.display = display;
@@ -10,14 +10,26 @@ function setEleDisplay(id, display) {
 function showEle(id) { setEleDisplay(id, 'block');  }
 function hideEle(id) { setEleDisplay(id, 'none');  }
 
-function showHeadlines() {
-    var headlinesURL = "https://newsapi.org/v2/top-headlines" +
-    "?" +
-    "country=us" +
-    "&" +
-    "category=entertainment" +
-    "&" +
-    "apiKey=4d17fa114b504a19a80b0c3c1c343e7b";
+function makeHeadline(title, output) {
+    output.push('<div class="headline">');
+    output.push('<a href="#article" onclick="showGIFs(\''
+     + encodeURIComponent(title).replace(/'/g, "\\'") 
+     + '\')">');
+    output.push(title);
+    output.push('</a>');
+    output.push('</div>');
+    return output;
+}
+
+function showHeadlines() {    
+    var headlinesURL = 
+    "http://newsapi.org/v2/top-headlines"
+    + "?"
+    + "country=us"
+    + "&"
+    + "category=entertainment"
+    + "&"
+    + "apiKey=4d17fa114b504a19a80b0c3c1c343e7b";
 
     var req = new XMLHttpRequest();
     req.open("GET", headlinesURL, true);
@@ -29,17 +41,45 @@ function showHeadlines() {
         for ( var articleIdx = 0; 
             articleIdx < newsData.articles.length; 
             articleIdx++) {
-
-        output.push('<div class="headline">');
-        output.push(newsData.articles[articleIdx].title);
-        output.push('</div>');
+        output = makeHeadline(newsData.articles[articleIdx].title, output);
       }
         document.getElementById('headlines').innerHTML = output.join('\r');
      };
 
     req.onerror = function () {
         hideEle('ajax-wait');
-        console.log('error');
+        document.getElementById('error').innerHTML =
+         'There was an error retrieving headlines. Please try again.';
+         showEle('error');
+    };
+
+    req.send();
+
+    showEle('ajax-wait');
+}
+
+function showGIFs(searchTerm) {
+    var gifsURL = "http://api.giphy.com/v1/gifs/search" +
+    "?" +
+    "rating=g" +
+    "&" +
+    "q=" + searchTerm +
+    "&" +
+    "apiKey=UdO7u798KoisXWYHJXVZ4cPvHjkL4HH0";
+
+    var req = new XMLHttpRequest();
+    req.open("GET", gifsURL, true);
+
+    req.onload = function () {
+        hideEle('ajax-wait');
+        console.log(req.responseText);
+     };
+
+     req.onerror = function () {
+        hideEle('ajax-wait');
+        document.getElementById('error').innerHTML =
+         'There was an error retrieving headlines. Please try again.';
+         showEle('error');
     };
 
     req.send();
